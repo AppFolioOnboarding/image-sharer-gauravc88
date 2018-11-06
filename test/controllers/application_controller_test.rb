9380d332bcce71bc263813072d2c2c9014b4833e
Gaurav Chitroda <gaurav.chitroda@appfolio.com>
2018-11-06 14:23:19 -0800

    gc/hz - change root_path to images new

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -4,6 +4,9 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_home
     get root_url
     assert_response :success
-    assert_select 'h1', count: 1
+    assert_select 'form', count: 1
+    assert_select 'form label', 'Image url'
+    assert_select 'form input[type=text]', count: 1
+    assert_select 'form input[type=submit]', count: 1
   end
 end
