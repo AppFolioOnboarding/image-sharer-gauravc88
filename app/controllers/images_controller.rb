698fa19650bd9fa75d871a00e79e22554f46bd8b
Gaurav Chitroda <gaurav.chitroda@appfolio.com>
2018-11-06 14:21:41 -0800

    gc/hz - add controller action, routes and view for images create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,20 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice] = 'Image saved'
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
