012f108755c0c2392d951f94b8310b17d9532a53
Gaurav Chitroda <gaurav.chitroda@appfolio.com>
2018-11-07 14:09:02 -0800

    gc - Updated root route to point to index page and corresponding tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -4,9 +4,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_home
     get root_url
     assert_response :success
-    assert_select 'form', count: 1
-    assert_select 'form label', 'Image url'
-    assert_select 'form input[type=text]', count: 1
-    assert_select 'form input[type=submit]', count: 1
+    assert_select 'h2', value: 'Stored Images'
+    assert_select 'a[href="/images/new"]', count: 1
   end
 end
